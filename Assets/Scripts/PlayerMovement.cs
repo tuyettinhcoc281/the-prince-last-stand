1699f43330645596a0c87507b49af2dafd1988f9
tuyettinhcoc281 <duybao281dh@gmail.com>
2025-02-26 17:10:40 +0700

    PlayerVer1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+public Animator animator;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+         animator.SetFloat("Vertical", movement.y);
+         animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
