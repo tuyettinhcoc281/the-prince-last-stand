c75bc06844bfb0b6f20b79912068b2b3e0189507
tuyettinhcoc281 <duybao281dh@gmail.com>
2025-03-13 16:05:29 +0700

    UpdatePlayer
    UpdatePlayer

--- Assets/Scripts/Enemies/EnemyAI.cs
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -4,41 +4,34 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    private enum State
-    {
+    [SerializeField] private float roamChangeDirFloat = 2f;
+
+    private enum State {
         Roaming
     }
 
     private State state;
     private EnemyPathfinding enemyPathfinding;
 
-    private void Awake()
-    {
+    private void Awake() {
         enemyPathfinding = GetComponent<EnemyPathfinding>();
         state = State.Roaming;
     }
 
-    private void Start()
-    {
+    private void Start() {
         StartCoroutine(RoamingRoutine());
     }
 
-    private IEnumerator RoamingRoutine()
-    {
+    private IEnumerator RoamingRoutine() {
         while (state == State.Roaming)
         {
             Vector2 roamPosition = GetRoamingPosition();
             enemyPathfinding.MoveTo(roamPosition);
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(roamChangeDirFloat);
         }
     }
 
-    private Vector2 GetRoamingPosition()
-    {
-        // new pos = Random.Range(-1f, 1f)
-        // new_pos = current_enemy_pos + new_pos
-        // if new_pos is outside of the screen, get a new pos
+    private Vector2 GetRoamingPosition() {
         return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
     }
-
 }
